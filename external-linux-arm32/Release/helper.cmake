a715b39114d45350c2b0807519b5881d181297fb
kukakong <kukakong@users.noreply.github.com>
2026-05-28 01:08:13 +0000

    feat: 生成代码文档和多平台可执行文件
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

---
+++ external-linux-arm32/Release/helper.cmake
@@ -0,0 +1,3 @@
+set(VULKAN_HEADERS_INSTALL_DIR "/workspace/external-linux-arm32/Release/Vulkan-Headers/build/install" CACHE STRING "")
+set(GOOGLETEST_INSTALL_DIR "/workspace/external-linux-arm32/Release/googletest/build/install" CACHE STRING "")
+set(VULKAN_LOADER_INSTALL_DIR "/workspace/external-linux-arm32/Release/Vulkan-Loader/build/install" CACHE STRING "")
---
+++ external-linux-arm64/Release/helper.cmake
@@ -0,0 +1,3 @@
+set(VULKAN_HEADERS_INSTALL_DIR "/workspace/external-linux-arm64/Release/Vulkan-Headers/build/install" CACHE STRING "")
+set(GOOGLETEST_INSTALL_DIR "/workspace/external-linux-arm64/Release/googletest/build/install" CACHE STRING "")
+set(VULKAN_LOADER_INSTALL_DIR "/workspace/external-linux-arm64/Release/Vulkan-Loader/build/install" CACHE STRING "")
